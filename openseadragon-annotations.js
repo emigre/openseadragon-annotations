9bda5f28a13d382ec5d05b87578a93f385b0be79
Gaspar <info@gasparrey.com>
2014-09-21 00:32:22 +0100

    Clean old code

--- openseadragon-annotations.js
+++ openseadragon-annotations.js
@@ -60,55 +60,6 @@
 
 		});
 
-/*
-		OSD.addEvent(self._viewer, 'animation', function() {
-
-			var myZoom = self._viewer.viewport.getZoom(true);
-			var myCanvas = sketchpad.paper();
-
-			//myCanvas.setViewBox(0, 0, 6600/myZoom, 6600/myZoom, true);
-			//myCanvas.setSize(myZoom*600, myZoom*600);
-
-			sketchpad.pen().width(myZoom*myStrokeWidth);
-
-			myCanvas.forEach(function (el) {
-			el.transform("");
-			el.scale(myZoom, myZoom, 0, 0);
-			el.attr('stroke-width', myZoom*myStrokeWidth);
-		});
-
-		myCanvas.setSize(myZoom*600, myZoom*600);
-
-		var editorDraw = document.getElementById('editor_draw'),
-			editorMove = document.getElementById('editor_move');
-
-		OSD.addEvent(editorDraw, 'click', function () {
-
-			OSD.removeClass(editorDraw, 'disabled');
-			OSD.addClass(editorDraw, 'selected');
-
-			OSD.addClass(editorMove, 'disabled');
-			OSD.removeClass(editorMove, 'selected');
-
-				sketchpad.editing(true);
-				self._viewer.setMouseNavEnabled(false);
-			});
-
-			OSD.addEvent(editorMove, 'click', function () {
-
-				OSD.removeClass(editorDraw, 'selected');
-				OSD.addClass(editorDraw, 'disabled');
-
-				OSD.addClass(editorMove, 'selected');
-				OSD.removeClass(editorMove, 'disabled');
-
-				sketchpad.editing("move");
-				self._viewer.setMouseNavEnabled(true);
-
-			});
-
-		}, 500);
-*/
 	};
 
 })(OpenSeadragon, Raphael);
