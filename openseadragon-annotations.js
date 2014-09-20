a3d0e5bd7bbe13d348a43090efd34c34a880bd47
Gaspar <info@gasparrey.com>
2014-09-21 00:26:55 +0100

    Create a overlay

--- openseadragon-annotations.js
+++ openseadragon-annotations.js
@@ -1,96 +1,109 @@
-(function (oS) {
-	
-	 oS.Viewer.prototype.initializeAnnotations = function (options) {
-	
-		var options = oS.extend({
-			// defaults
-		}, options || {});
-	
-		this.annotations = new oS.Annotations(options);
+(function (OSD, Raphael) {
 
-		console.log(this, oS.addEvent, this.annotations);
+	if (!OSD) {
+        throw new Error('OpenSeadragonAnnotations requires OpenSeadragon');
+    }
 
-		this.addHandler('open', this.annotations.addOverlays); 
-		
+	if (!Raphael) {
+        throw new Error('OpenSeadragonScalebar requires Raphael');
+    }
+
+	OSD.Viewer.prototype.initializeAnnotations = function (options) {
+
+		if (!this.annotations) {
+
+			var self = this;
+
+			var options = OSD.extend({
+				// defaults
+			}, options || {});
+
+			options.viewer = this;
+
+			this.annotations = new OSD.Annotations(options);
+
+			this.addHandler('open', OSD.delegate(self.annotations, self.annotations.addOverlays));
+
+		}
+
 	};
 
-	oS.Annotations = function () {
-	
-	};
-	
-	oS.Annotations.prototype.addOverlays = function () {
+	OSD.Annotations = function (options) {
 
-		console.log('add Overlays called');	
-		/*
-		zoom.viewer.setMouseNavEnabled(false);
+		var options = options || {};
+
+		this.viewer = options.viewer;
+
+	};
+
+	OSD.Annotations.prototype.addOverlays = function () {
 
 		var myDiv = document.createElement("div"),
-			myRect = new Seadragon.Rect(0, 0, 1, 1);
+			myRect = new OSD.Rect(0, 0, 1, 1);
 
-		myDiv.id = "myDrawings";
+		myDiv.id = "annotations";
 		myDiv.className = "overlay";
-		
-		zoom.viewer.drawer.addOverlay(myDiv, myRect);  
-		// TODO adjust the width and height to the measures of the drawing
-	  
-		// TODO do something to eliminate the need to do this...
-		setTimeout(function() {
 
-			var myStrokeWidth = 1;
-			
-			var sketchpad = Raphael.sketchpad("myDrawings", {
-				width: 6600,   
-				height: 6600, 
+		this.viewer.addOverlay(myDiv, myRect);
+		this.viewer.setMouseNavEnabled(false);
+
+		OSD.requestAnimationFrame(function (){
+
+			var sketchpad = Raphael.sketchpad("annotations", {
+				width: 6600,
+				height: 6600,
 				editing: true
 			});
-			sketchpad.pen().color("#f00");  
-			sketchpad.pen().width(myStrokeWidth); 
-			
-			oS.addEvent(zoom.viewer, 'animation', function() { 
 
-				var myZoom = zoom.viewer.viewport.getZoom(true);
-				var myCanvas = sketchpad.paper();
+			sketchpad.pen().color("#f00");
+			sketchpad.pen().width(1);
 
-				//myCanvas.setViewBox(0, 0, 6600/myZoom, 6600/myZoom, true);
-				//myCanvas.setSize(myZoom*600, myZoom*600);
-
-				sketchpad.pen().width(myZoom*myStrokeWidth);
-			
-				myCanvas.forEach(function (el) {
-				el.transform("");
-				el.scale(myZoom, myZoom, 0, 0);
-				el.attr('stroke-width', myZoom*myStrokeWidth);
-			});
-
-			myCanvas.setSize(myZoom*600, myZoom*600);
-				
 		});
-		
+
+/*
+		OSD.addEvent(self._viewer, 'animation', function() {
+
+			var myZoom = self._viewer.viewport.getZoom(true);
+			var myCanvas = sketchpad.paper();
+
+			//myCanvas.setViewBox(0, 0, 6600/myZoom, 6600/myZoom, true);
+			//myCanvas.setSize(myZoom*600, myZoom*600);
+
+			sketchpad.pen().width(myZoom*myStrokeWidth);
+
+			myCanvas.forEach(function (el) {
+			el.transform("");
+			el.scale(myZoom, myZoom, 0, 0);
+			el.attr('stroke-width', myZoom*myStrokeWidth);
+		});
+
+		myCanvas.setSize(myZoom*600, myZoom*600);
+
 		var editorDraw = document.getElementById('editor_draw'),
 			editorMove = document.getElementById('editor_move');
 
-		oS.addEvent(editorDraw, 'click', function () {
+		OSD.addEvent(editorDraw, 'click', function () {
 
-			oS.removeClass(editorDraw, 'disabled');
-			oS.addClass(editorDraw, 'selected');
+			OSD.removeClass(editorDraw, 'disabled');
+			OSD.addClass(editorDraw, 'selected');
 
-			oS.addClass(editorMove, 'disabled');
-			oS.removeClass(editorMove, 'selected');
+			OSD.addClass(editorMove, 'disabled');
+			OSD.removeClass(editorMove, 'selected');
 
 				sketchpad.editing(true);
-				zoom.viewer.setMouseNavEnabled(false);
+				self._viewer.setMouseNavEnabled(false);
 			});
 
-			oS.addEvent(editorMove, 'click', function () {
+			OSD.addEvent(editorMove, 'click', function () {
 
-				oS.removeClass(editorDraw, 'selected');
-				oS.addClass(editorDraw, 'disabled');
+				OSD.removeClass(editorDraw, 'selected');
+				OSD.addClass(editorDraw, 'disabled');
 
-				oS.addClass(editorMove, 'selected');
-				oS.removeClass(editorMove, 'disabled');
+				OSD.addClass(editorMove, 'selected');
+				OSD.removeClass(editorMove, 'disabled');
 
 				sketchpad.editing("move");
-				zoom.viewer.setMouseNavEnabled(true);
+				self._viewer.setMouseNavEnabled(true);
 
 			});
 
@@ -98,4 +111,4 @@
 */
 	};
 
-})(OpenSeadragon);
+})(OpenSeadragon, Raphael);
