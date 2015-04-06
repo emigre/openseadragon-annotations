2f59e707b5a880cbe99114178c268f78b63cf885
Gaspar Rey <info@gasparrey.com>
2015-04-06 13:54:00 +0100

    Remove stuff

--- openseadragon-annotations.js
+++ openseadragon-annotations.js
@@ -1,65 +1,46 @@
-(function (OSD, Raphael) {
+(function () {
 
-	if (!OSD) {
-        throw new Error('OpenSeadragonAnnotations requires OpenSeadragon');
+    if (!window.OpenSeadragon) {
+        throw new Error('OpenSeadragon Annotations requires OpenSeadragon');
     }
 
-	if (!Raphael) {
-        throw new Error('OpenSeadragonAnnotations requires Raphael');
-    }
+    OpenSeadragon.Viewer.prototype.initializeAnnotations = function (options) {
+        if (!this.annotations) {
+            var self = this;
+            var options = OpenSeadragon.extend({
+                // defaults
+            }, options || {});
+            options.viewer = this;
+            this.annotations = new OpenSeadragon.Annotations(options);
+            this.addHandler('open', OpenSeadragon.delegate(self.annotations, self.annotations.addOverlays));
+        }
 
-	OSD.Viewer.prototype.initializeAnnotations = function (options) {
+    };
 
-		if (!this.annotations) {
+    OpenSeadragon.Annotations = function (options) {
+        var options = options || {};
+        this.viewer = options.viewer;
+    };
 
-			var self = this;
+    OpenSeadragon.Annotations.prototype.addOverlays = function () {
+        var myDiv = document.createElement("div"),
+            myRect = new OpenSeadragon.Rect(0, 0, 1, 1);
+        myDiv.id = "annotations";
+        myDiv.className = "overlay";
+        this.viewer.addOverlay(myDiv, myRect);
+        this.viewer.setMouseNavEnabled(false);
+        // OpenSeadragon.requestAnimationFrame(function (){
 
-			var options = OSD.extend({
-				// defaults
-			}, options || {});
+            // var sketchpad = Raphael.sketchpad("annotations", {
+            //  width: 6600,
+            //  height: 6600,
+            //  editing: true
+            // });
 
-			options.viewer = this;
+            // sketchpad.pen().color("#f00");
+            // sketchpad.pen().width(1);
 
-			this.annotations = new OSD.Annotations(options);
+        // });
+    };
 
-			this.addHandler('open', OSD.delegate(self.annotations, self.annotations.addOverlays));
-
-		}
-
-	};
-
-	OSD.Annotations = function (options) {
-
-		var options = options || {};
-
-		this.viewer = options.viewer;
-
-	};
-
-	OSD.Annotations.prototype.addOverlays = function () {
-
-		var myDiv = document.createElement("div"),
-			myRect = new OSD.Rect(0, 0, 1, 1);
-
-		myDiv.id = "annotations";
-		myDiv.className = "overlay";
-
-		this.viewer.addOverlay(myDiv, myRect);
-		this.viewer.setMouseNavEnabled(false);
-
-		OSD.requestAnimationFrame(function (){
-
-			var sketchpad = Raphael.sketchpad("annotations", {
-				width: 6600,
-				height: 6600,
-				editing: true
-			});
-
-			sketchpad.pen().color("#f00");
-			sketchpad.pen().width(1);
-
-		});
-
-	};
-
-})(OpenSeadragon, Raphael);
+})();
