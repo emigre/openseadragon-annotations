54f6206f0c93379a420d0d371c4d5a3f35446820
Gaspar Rey <info@gasparrey.com>
2015-04-06 22:43:24 +0100

    Small tidy up

--- openseadragon-annotations.js
+++ openseadragon-annotations.js
@@ -4,31 +4,29 @@
         throw new Error('OpenSeadragon Annotations requires OpenSeadragon');
     }
 
-    OpenSeadragon.Viewer.prototype.initializeAnnotations = function (options) {
-        if (!this.annotations) {
-            var self = this;
-            var options = OpenSeadragon.extend({
-                // defaults
-            }, options || {});
-            options.viewer = this;
-            this.annotations = new OpenSeadragon.Annotations(options);
-            this.addHandler('open', OpenSeadragon.delegate(self.annotations, self.annotations.addOverlays));
-        }
-
-    };
-
-    OpenSeadragon.Annotations = function (options) {
+    var Annotations = function (options) {
         var options = options || {};
         this.viewer = options.viewer;
     };
 
-    OpenSeadragon.Annotations.prototype.addOverlays = function () {
+    Annotations.prototype.addOverlays = function () {
+        console.log('opening overlays');
         var myDiv = document.createElement("div"),
             myRect = new OpenSeadragon.Rect(0, 0, 1, 1);
         myDiv.id = "annotations";
         myDiv.className = "overlay";
+        console.log(this);
         this.viewer.addOverlay(myDiv, myRect);
         this.viewer.setMouseNavEnabled(false);
+    };
+
+    OpenSeadragon.Viewer.prototype.initializeAnnotations = function (options) {
+        if (!(this.hasOwnProperty('annotations') && this.annotations instanceof Annotations)) {
+            var options = OpenSeadragon.extend({ viewer: this }, options || {});
+            this.annotations = new Annotations(options);
+            this.addHandler('open', OpenSeadragon.delegate(this.annotations, this.annotations.addOverlays));
+        }
+        return this.annotations;
     };
 
 })();
