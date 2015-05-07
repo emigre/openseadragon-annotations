fe35d9516c91c81c9af971eb19d644f6fb6d41f8
Gaspar Rey <info@gasparrey.com>
2015-05-07 16:10:29 +0100

    Make the new controls switch between drawing mode and move mode

--- openseadragon-annotations.js
+++ openseadragon-annotations.js
@@ -4,29 +4,28 @@
         throw new Error('OpenSeadragon Annotations requires OpenSeadragon');
     }
 
+    var Modes = {
+        MOVE: 0,
+        DRAW: 1
+    };
+
     var Annotations = function (options) {
         var options = options || {};
         this.viewer = options.viewer;
+        this.mode = Modes.MOVE;
+        this.action = null;
+        this.controls = [];
     };
 
     Annotations.prototype.addOverlays = function () {
         var width = this.viewer.viewport.homeBounds.width;
         var height = this.viewer.viewport.homeBounds.height;
         var rect = new OpenSeadragon.Rect(0, 0, width, height);
-        //this.viewer.setMouseNavEnabled(false);
         this.el = new Overlay('annotations');
         this.svg = new Svg();
         this.el.appendChild(this.svg);
         this.viewer.addOverlay(this.el, rect);
-
-        this.svg.addEventListener('click', function (e) {
-            var x = e.offsetX / this.el.clientWidth;
-            var y = e.offsetY / this.el.clientHeight;
-            this.addCircle(x, y);
-        }.bind(this), false);
-
         this.addNewControls();
-
         this.viewer.addHandler('animation', this.refresh.bind(this));
         this.viewer.addHandler('open', this.refresh.bind(this));
     };
@@ -37,22 +36,53 @@
             srcRest: '/OpenSeadragonAnnotations/img/move_rest.png',
             srcGroup: '/OpenSeadragonAnnotations/img/move_grouphover.png',
             srcHover: '/OpenSeadragonAnnotations/img/move_hover.png',
-            srcDown: '/OpenSeadragonAnnotations/img/move_pressed.png'
+            srcDown: '/OpenSeadragonAnnotations/img/move_pressed.png',
+            onClick: this.setMoveMode.bind(this)
         });
         this.addNewControl({
             tooltip: 'Draw',
             srcRest: '/OpenSeadragonAnnotations/img/draw_rest.png',
             srcGroup: '/OpenSeadragonAnnotations/img/draw_grouphover.png',
             srcHover: '/OpenSeadragonAnnotations/img/draw_hover.png',
-            srcDown: '/OpenSeadragonAnnotations/img/draw_pressed.png'
+            srcDown: '/OpenSeadragonAnnotations/img/draw_pressed.png',
+            onClick: this.setDrawingMode.bind(this)
         });
     };
 
     Annotations.prototype.addNewControl = function (options) {
         var btn = new OpenSeadragon.Button(options);
+        this.controls.push(btn);
+        btn.addHandler('click', function () {
+            for (var i = 0; i < this.controls.length; i++) {
+                if (this.controls[i] === btn) {
+                    this.controls[i].imgDown.style.visibility = 'visible';
+                    this.controls[i].imgRest.style.visibility = 'hidden';
+                } else {
+                    this.controls[i].imgDown.style.visibility = 'hidden';
+                    this.controls[i].imgRest.style.visibility = 'visible';
+                }
+            }
+        }.bind(this));
         this.viewer.addControl(btn.element, {
-            anchor: OpenSeadragon.ControlAnchor.TOP_LEFT
+            anchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT
         });
+    };
+
+    Annotations.prototype.setDrawingMode = function () {
+        this.viewer.setMouseNavEnabled(false);
+        this.mode = Modes.DRAW;
+        this.action = function (e) {
+            var x = e.offsetX / this.el.clientWidth;
+            var y = e.offsetY / this.el.clientHeight;
+            this.addCircle(x, y);
+        }.bind(this);
+        this.svg.addEventListener('click', this.action, false);
+    };
+
+    Annotations.prototype.setMoveMode = function () {
+        this.viewer.setMouseNavEnabled(true);
+        this.mode = Modes.MOVE;
+        this.svg.removeEventListener('click', this.action, false);
     };
 
     Annotations.prototype.add = function (element) {
