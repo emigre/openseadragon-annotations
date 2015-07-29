6e6db81e29d6e4ab3d9c027a6d4f94524f40876e
Gaspar Rey <info@gasparrey.com>
2015-07-29 21:41:57 +0100

    Reorganize the annotations file

--- src/annotations.js
+++ src/annotations.js
@@ -10,20 +10,25 @@ export default {
             state: move.initialize(),
             controls: controls.initialize({
                 imagePath: options.imagePath || ''
+            }),
+            overlay: overlay.initialize({
+                viewer: options.viewer
             })
         }, options);
+
         this.controls.addHandler('add', function (button) {
             this.viewer.addControl(button.element, {
                 anchor: OpenSeadragon.ControlAnchor.BOTTOM_LEFT
             });
         }.bind(this));
-        return this;
-    },
-
-    onOpen: function () {
-        this.overlay = overlay.initialize({
-            viewer: this.viewer
-        });
+        this.controls.add('move', true).addHandler('click', function () {
+            this.viewer.setMouseNavEnabled(true);
+            this.state = move.initialize();
+        }.bind(this));
+        this.controls.add('draw').addHandler('click', function () {
+            this.viewer.setMouseNavEnabled(false);
+            this.state = draw.initialize();
+        }.bind(this));
 
         this.viewer.addHandler('animation', function () {
             var width = this.overlay.el.clientWidth;
@@ -32,25 +37,12 @@ export default {
             var svg = this.overlay.el.querySelector('svg');
             svg.setAttribute('viewPort', viewPort);
         }.bind(this));
-
-        this.controls.add('move', true).addHandler('click', function () {
-            this.viewer.setMouseNavEnabled(true);
-            this.state = move.initialize();
-        }.bind(this));
-
-        this.controls.add('draw').addHandler('click', function () {
-            this.viewer.setMouseNavEnabled(false);
-            this.state = draw.initialize();
-        }.bind(this));
-
         this.overlay.el.addEventListener('mousedown', function (e) {
             this.state.handleMouseDown(e, this.overlay);
         }.bind(this), false);
-
         this.overlay.el.addEventListener('mouseup', function (e) {
             this.state.handleMouseUp(e, this.overlay);
         }.bind(this), false);
-
         return this;
     },
 
--- src/openseadragon-annotations.js
+++ src/openseadragon-annotations.js
@@ -3,7 +3,6 @@ import annotations from './annotations';
 if (!OpenSeadragon) { throw new Error('OpenSeadragon Annotations requires OpenSeadragon'); }
 
 OpenSeadragon.Viewer.prototype.initializeAnnotations = function (options) {
-    this.addHandler('open', annotations.onOpen.bind(annotations));
     this.annotations = this.annotations || annotations.initialize(OpenSeadragon.extend({ viewer: this }, options));
     return this;
 };
