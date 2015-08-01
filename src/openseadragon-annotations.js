3286ddeeff86dee49a0e8f4ce8f90796a5200a35
Gaspar Rey <info@gasparrey.com>
2015-08-01 20:11:50 +0100

    Copy images using copyfiles

--- src/openseadragon-annotations.js
+++ src/openseadragon-annotations.js
@@ -1,13 +1,5 @@
+import OpenSeadragon from "OpenSeadragon";
 import annotations from './annotations';
-
-import '../img/draw_grouphover.png';
-import '../img/draw_hover.png';
-import '../img/draw_pressed.png';
-import '../img/draw_rest.png';
-import '../img/move_grouphover.png';
-import '../img/move_hover.png';
-import '../img/move_pressed.png';
-import '../img/move_rest.png';
 
 if (!OpenSeadragon) { throw new Error('OpenSeadragon Annotations requires OpenSeadragon'); }
 
