d8700dd4f5c4cacef0431815ec3d8d68e94a0b1a
Gaspar Rey <info@gasparrey.com>
2016-08-05 18:30:03 +0200

    Remove index from controls

--- src/controls/index.js
+++
@@ -1,9 +0,0 @@
-import Draw from './Draw';
-import Move from './Move';
-
-const controls = [
-  Move,
-  Draw,
-];
-
-export default controls;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import OpenSeadragon, { Rect, ControlAnchor } from 'OpenSeadragon';
 import { h, render } from 'preact';
 import Annotations from './components/Annotations';
-import controls from './controls/';
+import Move from './controls/Move';
+import Draw from './controls/Draw';
 
 export default OpenSeadragon.Viewer.prototype.initializeAnnotations = function () {
   this.addHandler('open', () => {
     const size = this.viewport.homeBounds;
     const rect = new Rect(0, 0, size.width, size.height);
     this.addOverlay(render(<Annotations />), rect);
-    controls.forEach((Control) => {
+    [ Move, Draw ].forEach((Control) => {
       this.addControl(new Control().element, {
         anchor: ControlAnchor.BOTTOM_LEFT
       });
