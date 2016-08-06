47ec974485ff9bbac9cdbcb59a1b2dd93cca28c1
Gaspar Rey <info@gasparrey.com>
2016-08-06 16:05:39 +0200

    Changes to how the library is built

---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  debug: true,
+  watch: true,
+  resolve: {
+    extensions: ['', '.jsx', '.js'],
+  },
+  entry: path.join(__dirname, 'src/main.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'openseadragon-annotations.js',
+    libraryTarget: 'var',
+    library: ['OpenSeadragon', 'Viewer', 'prototype', 'initializeAnnotations'],
+    pathinfo: true,
+  },
+  externals: {
+    'OpenSeadragon': 'OpenSeadragon',
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.png$/, loader: 'url' },
+    ],
+  },
+};
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.jsx', '.js'],
+  },
+  entry: path.join(__dirname, 'src/main.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/',
+    filename: 'openseadragon-annotations.js',
+    libraryTarget: 'var',
+    library: ['OpenSeadragon', 'Viewer', 'prototype', 'initializeAnnotations'],
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+    new webpack.optimize.UglifyJsPlugin({ compressor: { warnings: false, screw_ie8: true } }),
+  ],
+  externals: {
+    'OpenSeadragon': 'OpenSeadragon',
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/ },
+      { test: /\.png$/, loader: 'url' },
+    ],
+  },
+};
