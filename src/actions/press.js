789d1244d4ec5f7534b269ab371a16519f420ca2
Gaspar Rey <info@gasparrey.com>
2017-02-21 23:31:37 +0000

    Remove the utils folder

--- src/actions/press.js
+++ src/actions/press.js
@@ -1,4 +1,19 @@
-import shapesFactory from '../utils/shapesFactory';
+const shapesFactory = {
+  getPath(x, y) {
+    return [
+      'path',
+      {
+        fill: 'none',
+        d: `M${x} ${y}`,
+        stroke: 'red',
+        'stroke-width': 3,
+        'stroke-linejoin': 'round',
+        'stroke-linecap': 'round',
+        'vector-effect': 'non-scaling-stroke',
+      },
+    ];
+  },
+};
 
 export default function press(x, y, Dispatcher, Store) {
   switch (Store.getMode()) {
